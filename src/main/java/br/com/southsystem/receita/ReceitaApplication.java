12c46e17c993e5f6dd20c9686f5331f29cc59c04
Ricardo Souza <ricardo.souza@iteris.com.br>
2021-04-01 11:08:38 -0300

    Criação do projeto

---
+++ src/main/java/br/com/southsystem/receita/ReceitaApplication.java
@@ -0,0 +1,13 @@
+package br.com.southsystem.receita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceitaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/southsystem/receita/ReceitaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.southsystem.receita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
